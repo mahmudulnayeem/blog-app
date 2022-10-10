009dcd05cee820eabe84f975bfb750fe58f5d8fd
mahmudulnayeem <hasan@basikmarketing.in>
2022-10-10 16:40:04 +0600

    added title to blog page

--- pages/blogs/[id]/index.tsx
+++ pages/blogs/[id]/index.tsx
@@ -1,9 +1,15 @@
 import { Divider, Text, TypographyStylesProvider } from "@mantine/core";
+import Head from "next/head";
 import Layout from "../../../components/Layout";
 const Blog = ({ data }: { data: any }) => {
   const blog: any = data[0];
   return (
     <Layout>
+      <Head>
+        <title>blog</title>
+        <meta name="description" content={blog.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Text weight={800} size={24} align="center">
         {blog?.title}
       </Text>
